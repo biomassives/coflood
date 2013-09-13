e501a80652bd4a4f0b463603863c9a38ab5cde4f
Scott Rainey <scottjrainey@gmail.com>
2013-09-13 11:58:41 -0600

    Added bootstrap and twitter feed

--- index.php
+++ index.php
@@ -1,16 +1,31 @@
-<html>
-<title>Colorado Flood Event Crowdmap </title>
-<bodY>
-#coflood<BR>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Scott Rainey :: 2013 Curriculum Vitae</title>
+  <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+</head>
+<body>
+  <section class="container">
+    <div class="row-fluid">
+      <div class="span8">
+        <b>Offcial Road Closure, Emergency Shelter Map</b><BR>
+        <iframe src="https://mapsengine.google.com/map/u/0/viewer?mid=zYYdhADI7PvQ.kzJSidMtEIqY" height=500 width=900></iframe>
+        <BR>
+        <a href="https://mapsengine.google.com/map/u/0/viewer?mid=zYYdhADI7PvQ.kzJSidMtEIqY">full screen</a>
 
-<b>Offcial Road Closure, Emergency Shelter Map</b><BR>
-<iframe src="https://mapsengine.google.com/map/u/0/viewer?mid=zYYdhADI7PvQ.kzJSidMtEIqY" height=500 width=900></iframe>
-<BR>
-<a href="https://mapsengine.google.com/map/u/0/viewer?mid=zYYdhADI7PvQ.kzJSidMtEIqY">full screen</a>
+        <BR><BR>
+        <h2>messages sent to alerts@coflood.us go to this map</h2>
 
-<BR><BR>
-<h2>messages sent to alerts@coflood.us go to this map</h2>
-
-<iframe src=http://frontrangeflood.crowdmap.com width=900 height=500></iframe>
-<BR>
-<a href=http://frontrangeflood.crowdmap.com>full screen</a>
+        <iframe src=http://frontrangeflood.crowdmap.com width=900 height=500></iframe>
+        <BR>
+        <a href=http://frontrangeflood.crowdmap.com>full screen</a>
+      </div>
+      <div class="span4">
+        <a class="twitter-timeline"  href="https://twitter.com/search?q=%23coflood"  data-widget-id="378577294802956288">Tweets about "#coflood"</a>
+        <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+      </div>
+    </div>
+  </section>
+</body>
+</html>
