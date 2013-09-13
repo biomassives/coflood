22815de4aeea89eb3c48734e0e8c50fde940ff97
G. Willson <greg@biomassiv.es>
2013-09-13 11:34:35 -0600

    first scratch
    s

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<title>Colorado Flood Event Crowdmap </title>
+<bodY>
+#coflood<BR>
+
+<b>Offcial Road Closure, Emergency Shelter Map</b><BR>
+<iframe src="https://mapsengine.google.com/map/u/0/viewer?mid=zYYdhADI7PvQ.kzJSidMtEIqY" height=500 width=900></iframe>
+<BR>
+<a href="https://mapsengine.google.com/map/u/0/viewer?mid=zYYdhADI7PvQ.kzJSidMtEIqY">full screen</a>
+
+<BR><BR>
+<h2>messages sent to alerts@coflood.us go to this map</h2>
+
+<iframe src=http://frontrangeflood.crowdmap.com width=900 height=500></iframe>
+<BR>
+<a href=http://frontrangeflood.crowdmap.com>full screen</a>
